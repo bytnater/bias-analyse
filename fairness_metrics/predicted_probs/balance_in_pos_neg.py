e08f640dfdc7e8a9d66a43ecb385082d2e0917e3
Mick <mick04gerrits@gmail.com>
2025-06-17 15:33:46 +0200

    bit of the structere. but must go now

---
+++ fairness_metrics/predicted_probs/balance_in_pos_neg.py
@@ -0,0 +1,14 @@
+'''
+This file includes a class for the fairness metrics 'balance in positve/negative class
+
+author: Mick
+'''
+
+class balance_in_pos_neg:
+    def __init__(self, dataset, calc_pos = False, calc_neg = False):
+        self.calc_pos = calc_pos
+        self.calc_neg = calc_neg
+        print('done')
+
+    def print(self):
+        print(self.calc_neg, self.calc_pos)
