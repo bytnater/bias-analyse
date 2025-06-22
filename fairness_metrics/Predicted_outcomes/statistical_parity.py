c1ec3e2c263fd08be1665428ce4d311cec8de454
CasperKb <153619884+CasperKb@users.noreply.github.com>
2025-06-22 20:02:54 +0200

    changes to statistical parity and combining unconditional and conditional forms

--- fairness_metrics/Predicted_outcomes/statistical_parity.py
+++ fairness_metrics/Predicted_outcomes/statistical_parity.py
@@ -2,12 +2,13 @@ import torch
 import itertools
 import plotly.graph_objects as go
 
-class unconditional_statistical_parity:
-    def __init__(self, dataset, preset):
+class statistical_parity:
+    def __init__(self, dataset, params):
         """
         data: dataset Tensor
         predictions: Tensor of model predictions
         protected_attributes: List of protected attribute names
+        condition: a dictionary of attributes and specific value you want the individuals to have, leave empty for unconditional metric form
 
         dataset: contains data, i2c, c2i
         preset: contains all important data in a dict, note preset is a bad name for it but it is already all over the code
@@ -15,13 +16,14 @@ class unconditional_statistical_parity:
         self.dataset = dataset
 
 
-        prediction_column = preset.get('prediction_column', '')
+        prediction_column = params.get('prediction_column', '')
         assert prediction_column != '', 'This metric needs a prediction'
+        self.condition = params.get('condition', dict())
         predictions = self.dataset.data[:,self.dataset.c2i[prediction_column]]  ## extract column with predictions from datset
         self.predictions = (predictions > 0.8).float().squeeze()
         #Currently 80% certainty of model indicates a 1 prediction but can be changed
         
-        protected_values = preset.get('protected_values', torch.zeros(len(dataset.i2c), dtype=bool))
+        protected_values = params.get('protected_values', torch.zeros(len(dataset.i2c), dtype=bool))
         self.protected_attributes = [
             name for name, is_protected in zip(dataset.i2c, protected_values)
             if is_protected
@@ -35,13 +37,24 @@ class unconditional_statistical_parity:
             col_idx = self.dataset.c2i[attr]
             protected_col = self.dataset.data[:, col_idx]
 
-            unique_values = torch.unique(protected_col)
+            condition_mask = torch.ones(len(self.dataset.data), dtype=bool)
+            for cond_attr, cond_val in  self.condition.items():
+                cond_idx = self.dataset.c2i[cond_attr]
+                condition_mask &= (self.dataset.data[:, cond_idx] == cond_val)
+
+            filtered_protected_col = protected_col[condition_mask]
+            filtered_predictions = self.predictions[condition_mask]
+
+            unique_values = torch.unique(filtered_protected_col)
 
             probs = {}
             for val in unique_values:
-                mask = protected_col == val
-                preds = self.predictions[mask]
-                proportion = preds.mean().item()
+                mask = filtered_protected_col == val
+                preds = filtered_predictions[mask]
+                if len(preds) > 0:
+                    proportion = preds.mean().item()
+                else:
+                    proportion = float('nan')  # Handle empty groups
                 probs[int(val.item())] = proportion
 
             pair_diffs = {}
@@ -56,6 +69,14 @@ class unconditional_statistical_parity:
                 "pairwise_differences": pair_diffs,
                 "fair": is_fair
             }
+
+    def _get_column(self, feature):
+        return self.dataset.data[:,feature].squeeze(-1)
+
+    def _get_prediction_column(self, params):
+        prediction_column = params.get('prediction_column', '')
+        assert prediction_column != '', 'This metric needs a prediction'
+        return self.dataset.data[:,self.dataset.c2i[self.prediction_column]]
     
     def show(self, raw_results=False):
         if raw_results:
@@ -71,7 +92,7 @@ class unconditional_statistical_parity:
                 y=list(probs.values()),
             ))
             fig.update_layout(
-                title=f'Positive Prediction Rate by Group for "{attr_name}"',
+                title=f'Conditional Positive Prediction Rate by Group for "{attr_name}"',
                 xaxis_title='Group',
                 yaxis_title='Positive Prediction Rate',
                 yaxis=dict(range=[0, 1]),
