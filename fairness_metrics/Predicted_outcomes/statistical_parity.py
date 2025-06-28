a98ceff3270b5984a1233b0cbbb05b1a1b5059bd
Julius <julius@juliuss-laptop.home>
2025-06-28 19:02:40 +0200

    statistical_parity: Added code comments and formated using Black

--- fairness_metrics/Predicted_outcomes/statistical_parity.py
+++ fairness_metrics/Predicted_outcomes/statistical_parity.py
@@ -1,12 +1,13 @@
-'''
+"""
 This file contains the class for the metrics '(Un)conditional Statistical Parity'
 
 author: Casper K.
 date: Jun 2025
-'''
+"""
 
 import torch
 import plotly.graph_objects as go
+
 
 class statistical_parity:
     def __init__(self, dataset, params):
@@ -19,30 +20,28 @@ class statistical_parity:
         """
         self.dataset = dataset
 
-
-        prediction_column = params.get('prediction_column', '')
-        assert prediction_column != '', 'This metric needs a prediction'
-        self.condition = params.get('condition', dict())
-        predictions = self.dataset.data[:,self.dataset.c2i[prediction_column]]  ## extract column with predictions from datset
+        prediction_column = params.get("prediction_column", "")
+        assert prediction_column != "", "This metric needs a prediction"
+        self.condition = params.get("condition", dict())
+        predictions = self.dataset.data[
+            :, self.dataset.c2i[prediction_column]
+        ]  ## extract column with predictions from datset
         self.predictions = (predictions > 0.7).float().squeeze()
-        #Currently 80% certainty of model indicates a 1 prediction but can be changed
-        
-        protected_values = params.get('protected_values', torch.zeros(len(dataset.i2c), dtype=bool))
-        self.protected_attributes = [
-            name for name, is_protected in zip(dataset.i2c, protected_values)
-            if is_protected
-        ]
+        # Currently 80% certainty of model indicates a 1 prediction but can be changed
 
-        #calculations
+        protected_values = params.get("protected_values", torch.zeros(len(dataset.i2c), dtype=bool))
+        self.protected_attributes = [name for name, is_protected in zip(dataset.i2c, protected_values) if is_protected]
+
+        # calculations
         self.results = {}
         for attr in self.protected_attributes:
             col_idx = self.dataset.c2i[attr]
             protected_col = self.dataset.data[:, col_idx]
 
             condition_mask = torch.ones(len(self.dataset.data), dtype=bool)
-            for cond_attr, cond_val in  self.condition.items():
+            for cond_attr, cond_val in self.condition.items():
                 cond_idx = self.dataset.c2i[cond_attr]
-                condition_mask &= (self.dataset.data[:, cond_idx] == cond_val)
+                condition_mask &= self.dataset.data[:, cond_idx] == cond_val
 
             filtered_protected_col = protected_col[condition_mask]
             filtered_predictions = self.predictions[condition_mask]
@@ -56,44 +55,45 @@ class statistical_parity:
                 if len(preds) > 0:
                     proportion = preds.mean()
                 else:
-                    proportion = float('nan')
+                    proportion = float("nan")
                 probs[int(val)] = proportion
 
-            self.results[attr] = {
-                "group_probs": probs
-            }
+            self.results[attr] = {"group_probs": probs}
 
     def _get_column(self, feature):
-        return self.dataset.data[:,feature].squeeze(-1)
+        return self.dataset.data[:, feature].squeeze(-1)
 
     def _get_prediction_column(self, params):
-        prediction_column = params.get('prediction_column', '')
-        assert prediction_column != '', 'Statistical parity metrics needs a prediction'
-        return self.dataset.data[:,self.dataset.c2i[self.prediction_column]]
-    
+        prediction_column = params.get("prediction_column", "")
+        assert prediction_column != "", "Statistical parity metrics needs a prediction"
+        return self.dataset.data[:, self.dataset.c2i[self.prediction_column]]
+
     def show(self, raw_results=False) -> go.Figure:
         if raw_results:
             return self.results
         fig_list = []
         for attr_name in self.results:
             attr_data = self.results[attr_name]
-            probs = attr_data['group_probs']
-            
+            probs = attr_data["group_probs"]
+
             fig = go.Figure()
 
-            fig.add_trace(go.Bar(
-                x=[str(k) for k in probs.keys()],
-                y=list(probs.values()),
-            ))
+            fig.add_trace(
+                go.Bar(
+                    x=[str(k) for k in probs.keys()],
+                    y=list(probs.values()),
+                )
+            )
             fig.update_layout(
                 title=f'(Conditional) Positive Prediction Rate by Group for "{attr_name}"',
-                xaxis_title='Group',
-                yaxis_title='(Conditional) Positive Prediction Rate',
+                xaxis_title="Group",
+                yaxis_title="(Conditional) Positive Prediction Rate",
                 yaxis=dict(range=[0, 1]),
-                bargap=0.2
+                bargap=0.2,
             )
 
             fig_list.append(fig)
         return fig_list
-    
-print('loaded Statistical parity class')
+
+
+print("loaded Statistical parity class")
