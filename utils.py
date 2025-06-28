4ac5c8d323ac0893292e2efa33732928cde5c040
Mick <mick04gerrits@gmail.com>
2025-06-28 18:14:36 +0200

    better file upload, more detailed intro,

--- utils.py
+++ utils.py
@@ -1,21 +1,31 @@
 import pandas as pd
 import torch
+from io import BytesIO
 
 DATA_PATH_SYNTH = 'data/synth_data.csv'
 SAVED_PRESET_PATH = 'presets/'
 SAVED_DATASET_PATH = 'data/'
 RESEVERD_PRESET = 'session_save.pt'
 
-def load_csv_to_torch(path=DATA_PATH_SYNTH):
-    df = pd.read_csv(path)
+
+def load_csv_to_torch(path=None, upload_widget=None):
+    if upload_widget is not None:
+        uploaded_file = upload_widget[0]
+        content = uploaded_file.content
+        df = pd.read_csv(BytesIO(content))
+    elif path is not None:
+        df = pd.read_csv(path)
+    else:
+        raise ValueError("Either a file path or a file upload widget must be provided.")
+
     i2c = df.columns.tolist()
     c2i = {c: i for i, c in enumerate(i2c)}
     data_torch = torch.from_numpy(df.values)
     return data_torch, (i2c, c2i)
 
 class Dataset():
-    def __init__(self, PATH):
-        data, (i2c, c2i) = load_csv_to_torch(PATH)
+    def __init__(self, PATH=None, upload_widget=None):
+        data, (i2c, c2i) = load_csv_to_torch(PATH, upload_widget)
         self.data = data
         self.i2c = i2c
         self.c2i = c2i
